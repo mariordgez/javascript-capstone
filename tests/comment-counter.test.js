af833781957d133f70bab233c7b5d8418023425d
mariordgez <mariordgez@hotmail.com>
2021-09-10 12:28:52 -0700

    Fix linter errors

--- tests/comment-counter.test.js
+++ tests/comment-counter.test.js
@@ -5,8 +5,7 @@ import { getCommentCount } from '../src/modules/counters';
 
 describe('Gets correct count of comments', () => {
   test('Comment count equals to 3', () => {
-    document.body.innerHTML =
-      '<div>' + '  <ul class="container"></li>' + '</div>';
+    document.body.innerHTML = '<div><ul class="container"></li></div>';
     const commentsContainer = document.querySelector('.container');
     const item1 = { item_id: '1', likes: 50, id: 1 };
     const item2 = { item_id: '2', likes: 500, id: 2 };
@@ -14,15 +13,14 @@ describe('Gets correct count of comments', () => {
     const commentsArray = [item1, item2, item3];
     getCommentCount(commentsContainer, commentsArray);
 
-    expect(commentsContainer.innerHTML).toBe(`Comments (3)`);
+    expect(commentsContainer.innerHTML).toBe('Comments (3)');
   });
   test('Comment count equals to 0', () => {
-    document.body.innerHTML =
-      '<div>' + '  <ul class="container"></li>' + '</div>';
+    document.body.innerHTML = '<div><ul class="container"></li></div>';
     const commentsContainer = document.querySelector('.container');
     const commentsArray = [];
     getCommentCount(commentsContainer, commentsArray);
 
-    expect(commentsContainer.innerHTML).toBe(`Comments (0)`);
+    expect(commentsContainer.innerHTML).toBe('Comments (0)');
   });
 });
--- tests/like-counter.test.js
+++ tests/like-counter.test.js
@@ -17,12 +17,10 @@ describe('Gets correct count of items based on a transformed array', () => {
 
 describe('Gets correct count of likes based on the item', () => {
   test('likeCount equals to 50', () => {
-    document.body.innerHTML =
-      '<div>' + '  <ul class="container"></li>' + '</div>';
+    document.body.innerHTML = '<div><ul class="container"></li></div>';
     const likesContainer = document.querySelector('.container');
     const likeBtn = document.createElement('div');
-    likeBtn.innerHTML =
-      '<button id="1" class= "like-btn"><i class="fas fa-heart"></i></button>';
+    likeBtn.innerHTML = '<button id="1" class= "like-btn"><i class="fas fa-heart"></i></button>';
     likeBtn.id = '1';
     const item1 = { item_id: '1', likes: 50, id: 1 };
     const item2 = { item_id: '2', likes: 500, id: 2 };
@@ -33,12 +31,10 @@ describe('Gets correct count of likes based on the item', () => {
     expect(likesContainer.innerHTML).toBe('50 likes');
   });
   test('likeCount equals to 500', () => {
-    document.body.innerHTML =
-      '<div>' + '  <ul class="container"></li>' + '</div>';
+    document.body.innerHTML = '<div><ul class="container"></li></div>';
     const likesContainer = document.querySelector('.container');
     const likeBtn = document.createElement('div');
-    likeBtn.innerHTML =
-      '<button id="1" class= "like-btn"><i class="fas fa-heart"></i></button>';
+    likeBtn.innerHTML = '<button id="1" class= "like-btn"><i class="fas fa-heart"></i></button>';
     likeBtn.id = '2';
     const item1 = { item_id: '1', likes: 50, id: 1 };
     const item2 = { item_id: '2', likes: 500, id: 2 };
@@ -49,12 +45,10 @@ describe('Gets correct count of likes based on the item', () => {
     expect(likesContainer.innerHTML).toBe('500 likes');
   });
   test('likeCount not equal to 500', () => {
-    document.body.innerHTML =
-      '<div>' + '  <ul class="container"></li>' + '</div>';
+    document.body.innerHTML = '<div><ul class="container"></li></div>';
     const likesContainer = document.querySelector('.container');
     const likeBtn = document.createElement('div');
-    likeBtn.innerHTML =
-      '<button id="1" class= "like-btn"><i class="fas fa-heart"></i></button>';
+    likeBtn.innerHTML = '<button id="1" class= "like-btn"><i class="fas fa-heart"></i></button>';
     likeBtn.id = '3';
     const item1 = { item_id: '1', likes: 500, id: 1 };
     const item2 = { item_id: '2', likes: 500, id: 2 };
