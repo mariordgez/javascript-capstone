a6af25cad18dc6cfbf96931dd6814386b4d39ee8
mariordgez <mariordgez@hotmail.com>
2021-09-10 06:28:21 -0700

    Add font awesome to project

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import buildShows from './modules/buildShows.js';
 import { showsCount } from './modules/counters.js';
 import getShows from './modules/getShows.js';
 import modalStructure from './modules/modal.js';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 
 const itemsTitle = document.querySelector('.items-title');
 const allShowsContainer = document.querySelector('.shows-list');
 
 window.addEventListener('load', async () => {
   const shows = await getShows();
-  const sixShows = shows.slice(0, 6);
+  const sixShows = shows.slice(0, 20);
 
   itemsTitle.innerHTML = `TV SHOWS (${showsCount(sixShows)})`;
   await buildShows(sixShows, allShowsContainer);
